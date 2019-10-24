814a63d5a5a3ca610d991236594c2909569982ae
Prescott White <prgwhite@ucsc.edu>
2019-10-24 15:10:05 -0700

    Initial commit

---
+++ app/src/main/java/com/cse118/tanabesuganodiagramslider/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cse118.tanabesuganodiagramslider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
