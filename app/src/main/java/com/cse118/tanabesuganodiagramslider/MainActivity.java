d3df6804b6f0b29fde26fbf8675a70826df65f7c
Prescott White <prgwhite@ucsc.edu>
2019-10-30 20:29:32 -0700

    Hide views when moving progress bar to 'focus' user on graph. Add more mock settings and scrollview at bottom

--- app/src/main/java/com/cse118/tanabesuganodiagramslider/MainActivity.java
+++ app/src/main/java/com/cse118/tanabesuganodiagramslider/MainActivity.java
@@ -8,8 +8,13 @@ import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -17,60 +22,93 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner diagramDropdown;
-    GraphView graph;
-    LineGraphSeries<DataPoint> seek_series;
+    Spinner mDiagramDropdown;
+
+    GraphView mGraph;
+    LineGraphSeries<DataPoint> mSeek_series;
+
+    SeekBar mSeekBar;
+    EditText mEditText;
+    TextView mTextView;
+    RadioButton mRadio1;
+    RadioButton mRadio2;
+    Switch mSwitch1;
+    Switch mSwitch2;
+    ToggleButton mToggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        diagramDropdown = findViewById(R.id.select_diagram);
+        mDiagramDropdown = findViewById(R.id.select_diagram);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.diagrams_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        diagramDropdown.setAdapter(adapter);
+        mDiagramDropdown.setAdapter(adapter);
 
-        graph = (GraphView) findViewById(R.id.graph);
+        mGraph = (GraphView) findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
                 new DataPoint(0, 10),
                 new DataPoint(8, 20),
                 new DataPoint(15, 30),
                 new DataPoint(25, 40),
-                new DataPoint(35, 50)
+                new DataPoint(40, 50)
         });
         LineGraphSeries<DataPoint> series2 = new LineGraphSeries<>(new DataPoint[] {
                 new DataPoint(0, 50),
                 new DataPoint(10, 60),
                 new DataPoint(20, 70),
                 new DataPoint(25, 75),
-                new DataPoint(35, 75)
+                new DataPoint(40, 75)
         });
-        graph.addSeries(series);
-        graph.addSeries(series2);
+        mGraph.addSeries(series);
+        mGraph.addSeries(series2);
+
+        mSeekBar = findViewById(R.id.seek_x);
+
+        mTextView = findViewById(R.id.textView);
+        mEditText = findViewById(R.id.editText);
+        mRadio1 = findViewById(R.id.radioButton);
+        mRadio2 = findViewById(R.id.radioButton2);
+        mSwitch1 = findViewById(R.id.switch1);
+        mSwitch2 = findViewById(R.id.switch2);
+        mToggleButton = findViewById(R.id.toggleButton);
+        final View[] views = {mDiagramDropdown, mTextView, mEditText, mRadio1, mRadio2, mSwitch1, mSwitch2, mToggleButton};
 
         SeekBar seekBar = findViewById(R.id.seek_x);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                graph.removeSeries(seek_series);
-                seek_series = new LineGraphSeries<>(new DataPoint[] {
+                mGraph.removeSeries(mSeek_series);
+                mSeek_series = new LineGraphSeries<>(new DataPoint[] {
                         new DataPoint(progress, 0),
-                        new DataPoint(progress, graph.getY())
+                        new DataPoint(progress, 75)
                 });
-                graph.addSeries(seek_series);
+                mGraph.addSeries(mSeek_series);
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
+                hideViews(views);
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                showViews(views);
             }
         });
+    }
+
+    void hideViews(View[] views) {
+        for (int i = 0; i < views.length; i++) {
+            views[i].setVisibility(View.INVISIBLE);
+        }
+    }
+
+    void showViews(View[] views) {
+        for (int i = 0; i < views.length; i++) {
+            views[i].setVisibility(View.VISIBLE);
+        }
     }
 }
